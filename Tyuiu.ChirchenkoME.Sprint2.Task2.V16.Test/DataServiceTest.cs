b22c60594499996d46726f940a3201edaeb0e3f1
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-10-04 14:48:54 +0300

    Task2V16

---
+++ Tyuiu.ChirchenkoME.Sprint2.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.ChirchenkoME.Sprint2.Task2.V16.Lib;
+
+namespace Tyuiu.ChirchenkoME.Sprint2.Task2.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 6;
+            int y = 4;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = false;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
