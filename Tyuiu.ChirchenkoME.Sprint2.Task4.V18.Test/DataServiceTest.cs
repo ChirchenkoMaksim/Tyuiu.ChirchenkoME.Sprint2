0e73c8bbf127b03afc147fbb892f6f2d914f82a5
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-10-04 16:38:27 +0300

    Task4V18

---
+++ Tyuiu.ChirchenkoME.Sprint2.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ChirchenkoME.Sprint2.Task4.V18.Lib;
+
+namespace Tyuiu.ChirchenkoME.Sprint2.Task4.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 6;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint2.Task4.V18/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Tyuiu.ChirchenkoME.Sprint2.Task4.V18;
+using Tyuiu.ChirchenkoME.Sprint2.Task4.V18.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Чирченко М Е | ИCПБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Базовые                                                           *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнил: Чирченко М Е | ИСПБ-25-1                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x; double y;
+
+        Console.WriteLine("введите значение (x)");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите значение (y)");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("X= " + x);
+        Console.WriteLine("Y= " + y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Результат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        double res = ds.Calculate(x, y);
+
+        Console.WriteLine("Значение функции = " + res);
+
+
+        Console.ReadKey();
+
+
+    }
+}
