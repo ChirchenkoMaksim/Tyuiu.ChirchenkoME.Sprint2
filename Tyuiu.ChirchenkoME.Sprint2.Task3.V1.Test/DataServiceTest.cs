27baa30ca4c31a4d6da4dcef1ffaea527a9ccd8b
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-10-04 15:21:03 +0300

    Task3V1

---
+++ Tyuiu.ChirchenkoME.Sprint2.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.ChirchenkoME.Sprint2.Task3.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint2.Task3.V1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.ChirchenkoME.Sprint2.Task3.V1.Lib;
+namespace Tyuiu.ChirchenkoME.Sprint2.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Чирченко М Е | ИCПБ-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Чирченко М Е | ИСПБ-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* УСЛОВИЕ: Написать программу, которая вычисляет требуемое значение       *");
+            Console.WriteLine("* функции Y с использованием вложенных оператор if-else, где пользователь *");
+            Console.WriteLine("*вводит значение переменной X с клавиатуры. Округлить полученное значение *");
+            Console.WriteLine("*до трех знаков после запятой;                                            *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(x);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
